80f4b5defa9992cfff5eda33665a1835d583182e
Nabil AbuShaban <nabil.abushaban@gmail.com>
2024-07-08 11:36:56 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "frame-ancestors 'self' https://*.example.com https://example.com http://localhost:3000",
+              `frame-ancestors verify.walletconnect.org verify.walletconnect.com;`,
           },
         ],
       },
